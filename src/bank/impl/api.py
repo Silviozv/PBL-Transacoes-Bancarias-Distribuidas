de034dffef0db7c711897d2e66a4f87d82a2d653
Silvio <silviozv.oli@gmail.com>
2024-06-02 15:36:00 -0300

    ✨feat (API): Criação de caminhos da API.
    Criação dos seguintes caminhos: Checar se a API está online, registrar usuário, registrar conta, depositar, sacar, e retornar todos os dados armazenados no banco.

--- src/bank/impl/api.py
+++ src/bank/impl/api.py
@@ -3,17 +3,66 @@ import impl.bank_impl as impl
 
 app = Flask(__name__)
 
-@app.route('/',methods=['HEAD'])
+
+@app.route('/', methods=['HEAD'])
 def check_connection():
 
     return '', 200
 
-@app.route('/register/<string:type_account>/<string:names_user>/<string:cpfs>',methods=['POST'])
-def register_account(type_account: str, names_user: str, cpfs: str):
+
+@app.route('/show', methods=['GET'])
+def show_all():
+
+    return jsonify(impl.show_all()), 200
 
 
+@app.route('/register/user/<string:cpf>/<string:name>', methods=['POST'])
+def register_user(cpf: str, name: str):
 
-    return '', 200
+    data_user = {"CPF": cpf, "Nome": name}
+    response = impl.register_user(data_user)
+
+    if response["Bem sucedido"] == True:
+        return jsonify(response), 200
+    else:
+        return jsonify(response), 404
+
+
+@app.route('/register/account/<string:type_account>/<string:cpfs>', methods=['POST'])
+def register_account(type_account: str, cpfs: str):
+
+    type_account = type_account.split("-")
+    cpfs = cpfs.split('-')
+    data_account = {"Tipo de conta": type_account, "CPFs": cpfs}
+    response = impl.register_account(data_account)
+
+    if response["Bem sucedido"] == True:
+        return jsonify(response), 200
+    else:
+        return jsonify(response), 404
+
+
+@app.route('/deposit/<string:id>/<string:value>', methods=['PATCH'])
+def deposit_value(id: str, value: str):
+
+    response = impl.deposit(id, value)
+
+    if response["Bem sucedido"] == True:
+        return jsonify(response), 200
+    else:
+        return jsonify(response), 404
+
+
+@app.route('/withdraw/<string:id>/<string:value>', methods=['PATCH'])
+def withdraw_value(id: str, value: str):
+
+    response = impl.withdraw(id, value)
+
+    if response["Bem sucedido"] == True:
+        return jsonify(response), 200
+    else:
+        return jsonify(response), 404
+
 
 def start():
-    app.run(port=5070,host='0.0.0.0')
+    app.run(port=5070, host='0.0.0.0')
