f3a7034f48a5c5ad76e9770393d1045819a3f4a1
Silvio <silviozv.oli@gmail.com>
2024-06-02 22:14:59 -0300

    ✨feat (Database): Declarando atributos e métodos.
    Declarando atributo do IP da máquina que está rodando o banco. Criando método para encontrar id da conta pela chave PIX.

--- src/model/database.py
+++ src/model/database.py
@@ -1,12 +1,26 @@
 import threading
-
+import socket
 
 class Database:
     accounts: dict
     users: dict
+    ip_bank: str
+    lock: object
 
     def __init__(self):
         self.accounts = {}
         self.users = {}
 
+        self.ip_bank = socket.gethostbyname(socket.gethostname())
+
         self.lock = threading.Lock()
+
+    def find_account_by_key(self, key: str) -> dict:
+        data_search = {"Conta encontrada": False}
+
+        for id in self.accounts.keys():
+            if self.accounts[id].key == key:
+                data_search["ID conta"] = id
+                data_search["Conta encontrada"] = True
+
+        return data_search
