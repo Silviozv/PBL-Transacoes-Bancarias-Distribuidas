87856b9f3fa34c2663b3a05a3dca68ebc36ac74a
Silvio <silviozv.oli@gmail.com>
2024-07-03 23:02:14 -0300

    📚docs (Utils): Documentação.

--- src/bank/utils/outline_requests.py
+++ src/bank/utils/outline_requests.py
@@ -1,15 +1,50 @@
+""" 
+Módulo contendo as funções de esboço de envio de requisições HTTP.
+"""
+
 import threading
 import requests
 
 
 def create_result_structure(quantity: int) -> dict:
+    """
+    Cria estrutura para requisições separadas em threads indicarem quando foi terminado o envio 
+    da mensagens.
+
+    :param quantity: Quantidade de requisições que serão feitas.
+    :type quantity: int
+    :return: Estrutura com as indicações para as requisições setarem após seu término.
+    :rtype: dict
+    """
+
     result_dict = {}
     for i in range(quantity):
         result_dict[i] = {"Terminado": False, "Resposta": None}
+
     return result_dict
 
 
 def send_request(database: object, url: str, ip_bank: str, data: dict, http_method: str, result_dict: dict, index: str):
+    """
+    Esboço para envio de uma requisição HTTP. Quando o envio é terminado é indicado na estrutura 
+    para processos em outros planos terem acesso ao término e ao retorno da requisição.
+
+    :param database: Representação do armazenamento do banco.
+    :type database: object
+    :param url: URL da requisição.
+    :type url: str
+    :param ip_bank: IP do banco que vai receber a requisição.
+    :type ip_bank: str
+    :param data: Dados a serem enviados com a requisição.
+    :type data: dict
+    :param http_method: Método HTTP da requisição.
+    :type http_method: str
+    :param result_dict: Estrutura para indicar o término do envio da requisição e o retorno dela.
+    :type result_dict: dict
+    :param index: Chave do processo atual na estrutura.
+    :type index: str
+    """
+
     try:
         if http_method == "GET":
             response = requests.get(url, json=data, timeout=5)
