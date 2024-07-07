0648d3f50f0d97e17926a09f544919f49203f3f3
Silvio <silviozv.oli@gmail.com>
2024-07-07 00:10:51 -0300

    📚docs (API): Documentação.

--- src/bank/impl/api.py
+++ src/bank/impl/api.py
@@ -1,3 +1,7 @@
+""" 
+Módulo contendo os caminhos a função relacionados a implementação da API.
+"""
+
 from flask import Flask, jsonify, request
 import threading
 import re
@@ -8,12 +12,20 @@ import impl.token_impl as token_impl
 import impl.package_impl as package_impl
 from model.database import Database
 
+
+""" 
+Declaração da API e do objeto contendo o armazenamento do banco.
+"""
 app = Flask(__name__)
 database = Database()
 
 
 @app.route('/ready_for_connection', methods=['GET'])
 def ready_for_connection():
+    """
+    Retorna se o banco está disponível para conexão.
+    """
+
     response = bank_impl.ready_for_connection(database)
 
     if response["Bem sucedido"] == True:
@@ -24,6 +36,9 @@ def ready_for_connection():
 
 @app.route('/register/user', methods=['POST'])
 def register_user():
+    """
+    Registra um usuário no armazenamento do banco.
+    """
 
     data_user = request.json
     response = register_impl.register_user(database, data_user)
@@ -36,6 +51,9 @@ def register_user():
 
 @app.route('/check/user', methods=['GET'])
 def check_user():
+    """
+    Checa se o usuário indicado está no armazenamento do banco.
+    """
 
     data_check = request.json
     response = bank_impl.check_user(database, data_check)
@@ -48,6 +66,9 @@ def check_user():
 
 @app.route('/register/account', methods=['POST'])
 def register_account():
+    """
+    Registra uma conta no armazenamento do banco.
+    """
 
     data_account = request.json
     response = register_impl.register_account(database, data_account)
@@ -60,6 +81,9 @@ def register_account():
 
 @app.route('/get/account/consortium', methods=['GET'])
 def get_account_consortium():
+    """
+    Retorna os dados das contas do usuário em todos os bancos do consórcio.
+    """
 
     data_user = request.json
     response = bank_impl.get_account_consortium(database, data_user)
@@ -72,6 +96,9 @@ def get_account_consortium():
 
 @app.route('/get/account/user', methods=['GET'])
 def get_account_by_user():
+    """
+    Retorna as contas do usuário indicado armazenadas no banco.
+    """
 
     data_user = request.json
     response = bank_impl.get_account_by_user(database, data_user)
@@ -84,6 +111,9 @@ def get_account_by_user():
 
 @app.route('/check/account/id', methods=['GET'])
 def check_account_by_id():
+    """
+    Checa se a conta indicada está no armazenamento do banco pelo ID.
+    """
 
     data_account = request.json
     response = bank_impl.check_account_by_id(database, data_account)
@@ -96,6 +126,9 @@ def check_account_by_id():
 
 @app.route('/deposit', methods=['PATCH'])
 def deposit_value():
+    """
+    Deposita um valor em uma conta do banco.
+    """
 
     data_deposit = request.json
     response = bank_impl.deposit(database, data_deposit)
@@ -108,6 +141,9 @@ def deposit_value():
 
 @app.route('/withdraw', methods=['PATCH'])
 def withdraw_value():
+    """
+    Saca um valor em uma conta do banco.
+    """
 
     data_withdraw = request.json
     response = bank_impl.withdraw(database, data_withdraw)
@@ -121,6 +157,10 @@ def withdraw_value():
 
 @app.route('/check_first_pass_token', methods=['GET'])
 def check_first_pass_token():
+    """
+    Checa se o token já está passando no sistema.
+    """
+
     response = token_impl.check_first_pass_token(database)
 
     if response["Token está no sistema"] == True:
@@ -131,6 +171,10 @@ def check_first_pass_token():
 
 @app.route('/token_pass', methods=['POST'])
 def token_pass():
+    """
+    Recebimento do token.
+    """
+
     data_token = request.json
     response = token_impl.receive_token(database, data_token)
 
@@ -142,6 +186,10 @@ def token_pass():
 
 @app.route('/alert_problem_detected', methods=['POST'])
 def alert_problem_detected():
+    """
+    Recebimento de alerta de problema detectado na passagem do token no sistema.
+    """
+
     data_alert = request.json
     response = token_impl.receive_problem_alert(database, data_alert)
 
@@ -153,6 +201,10 @@ def alert_problem_detected():
 
 @app.route('/request_package', methods=['PATCH'])
 def request_package():
+    """
+    Recebimento de um pacote de transferências.
+    """
+
     data_package = request.json
     response = package_impl.request_package(database, data_package)
 
@@ -164,8 +216,13 @@ def request_package():
 
 @app.route('/send_transfer', methods=['PATCH'])
 def send_transfer_value():
+    """
+    Recebimento de requisição para transferir um valor de uma conta.
+    """
+
     data_transfer = request.json
     response = bank_impl.send_transfer(database, data_transfer)
+
     if response["Bem sucedido"] == True:
         return jsonify(response), 200
     else:
@@ -174,8 +231,13 @@ def send_transfer_value():
 
 @app.route('/receive_transfer', methods=['PATCH'])
 def receive_transfer_value():
+    """
+    Recebimento de um valor para ser adicionado ao saldo bloqueado de uma conta.
+    """
+
     data_transfer = request.json
     response = bank_impl.receive_transfer(database, data_transfer)
+
     if response["Bem sucedido"] == True:
         return jsonify(response), 200
     else:
@@ -184,8 +246,14 @@ def receive_transfer_value():
 
 @app.route('/send_release_transfer', methods=['PATCH'])
 def send_release_transfer_value():
+    """
+    Recebimento de requisição para enviar aviso de liberação de valor no saldo bloqueado 
+    de uma conta.
+    """
+
     data_transfer = request.json
     response = bank_impl.send_release_transfer(database, data_transfer)
+
     if response["Bem sucedido"] == True:
         return jsonify(response), 200
     else:
@@ -194,8 +262,13 @@ def send_release_transfer_value():
 
 @app.route('/receive_release_transfer', methods=['PATCH'])
 def receive_release_transfer_value():
+    """
+    Recebimento de requisição para liberar o saldo bloqueado de uma conta.
+    """
+
     data_transfer = request.json
     response = bank_impl.receive_release_transfer(database, data_transfer)
+
     if response["Bem sucedido"] == True:
         return jsonify(response), 200
     else:
@@ -204,8 +277,13 @@ def receive_release_transfer_value():
 
 @app.route('/confirm_package_execution', methods=['PATCH'])
 def confirm_package_execution():
+    """
+    Recebimento da confirmação de execução de um pacote.
+    """
+
     data_confirm = request.json
     response = package_impl.confirm_package_execution(database, data_confirm)
+    
     if response["Bem sucedido"] == True:
         return jsonify(response), 200
     else:
@@ -214,6 +292,10 @@ def confirm_package_execution():
 
 @app.route('/check_it_has_token', methods=['GET'])
 def check_it_has_token():
+    """
+    Checa se o token está no banco.
+    """
+
     response = token_impl.check_it_has_token(database)
 
     if response["Bem sucedido"] == True:
@@ -223,6 +305,13 @@ def check_it_has_token():
 
 
 def start():
+    """
+    Inicialização da API. É exibido o IP atual do banco. É pedida a quantidade de 
+    bancos do consórcio, em seguida, é solicitado o IP de cada um. São iniciadas as
+    funções de contagem do tempo sem receber o token e de inicialização dos dados 
+    do banco, depois, a API é iniciada.
+    """
+
     print(f"\nIP atual: {database.ip_bank}")
 
     quantity = input("\nQuantidade de bancos do consórcio: ")
